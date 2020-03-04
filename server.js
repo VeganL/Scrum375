e0f6d2532330eb3e72a82f47342d1120d33ab1aa
VeganL <veganlroy@gmail.com>
2020-03-03 23:39:15 -0500

    20200303 2338 Update: Testing MariaDB Connector

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 // Declaring/importing necessary files and frameworks
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var mariadb = require("mariadb");
+var dbKey = require("../mariadb_info.json")
+var pool = mariadb.createPool(dbKey);
 
 // Instantiating an express instance
 var app = express();
 
 // Serves content on port 8080 from the directory named "public"
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(8080);
+
+app.post("/signin", function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    async function signin () {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const response = await conn.query("SELECT user_id, board_ids FROM accounts WHERE username='" + username + "' AND password='" + password + "';");
+            res.send(response); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+      
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.end();
+        }
+      }
+});
