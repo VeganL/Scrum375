e57272ecfccbf6c928d4afe04315a6bf3f72daa0
Nicholas Robbins <njr58@drexel.edu>
2020-03-17 01:26:52 -0400

    added board sorting functionality

--- public/Boards/boards.js
+++ public/Boards/boards.js
@@ -18,15 +18,187 @@ function getBoards() {
                 let modDateStr = modDate.toString().substring(4,10) + ', ' + modDate.toString().substring(11,15);
                 let createDateStr = createDate.toString().substring(4,10) + ', ' + createDate.toString().substring(11,15);
 
-                boardAreaStr += '<div class="board-card"><h3>' + board.board_name + '</h3><p>Last modified: ' + modDateStr + '</p><p>Created on: ' + createDateStr + '</p><p>Members: ' + String(board.member_amt) + '</p><p>Tasks: ' + String(board.task_amt) + '</p></div>';
+                boardAreaStr += '<div class="board-card"><h3>' + board.board_name + '</h3><p id="lastModified">Last modified: ' + modDateStr + '</p><p id="dateCreated">Created on: ' + createDateStr + '</p><p id="numMembers">Members: ' + String(board.member_amt) + '</p><p id="numTasks">Tasks: ' + String(board.task_amt) + '</p></div>';
             }
 
             boardArea.innerHTML = boardAreaStr;
+            sortBoards();
         });
 
         xmlHttp.open("POST", requestURL);
         xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xmlHttp.send("board_ids=" + boardIds);
     }
+}
+
+function populateBoardArea(boardsArr)
+{
+    let boardArea = document.getElementById('boardArea');
+    let boardAreaStr = '';
+    for(var i = 0; i<boardsArr.length; i++)
+    {
+        let board = boardsArr[i];
+        boardAreaStr += '<div class="board-card">' + board.innerHTML + '</div>';
+    }
+    boardArea.innerHTML = boardAreaStr;
+}
+
+function sortBoards(){
+    let sortBy = document.getElementById("sortBy").value;
+    let boardsArr = Array.from(document.getElementsByClassName("board-card"));
+
+    switch(sortBy)
+    {
+        case "lastModified":
+            boardsArr.sort(compareLastModified);
+            break;
+        case "dateCreated":
+            boardsArr.sort(compareDateCreated);
+            break;
+        case "numMembers":
+            boardsArr.sort(compareNumMembers);
+            break;
+        case "numTasks":
+            boardsArr.sort(compareNumTasks);
+            break;
+    }
+    populateBoardArea(boardsArr);
+}
+function compareLastModified(a,b)
+{
+    return compareDates(a.querySelector("#lastModified").textContent.substring(15),b.querySelector("#lastModified").textContent.substring(15));
+}
+function compareDateCreated(a,b)
+{
+    return compareDates(a.querySelector("#dateCreated").textContent.substring(12),b.querySelector("#dateCreated").textContent.substring(12));
+}
+function compareNumMembers(a,b)
+{
+    return compareNums(a.querySelector("#numMembers").textContent.substring(9),b.querySelector("#numMembers").textContent.substring(9));
+}
+function compareNumTasks(a,b)
+{
+    return compareNums(a.querySelector("#numTasks").textContent.substring(7),b.querySelector("#numTasks").textContent.substring(7));
+}
+
+function compareDates(date1, date2)
+{
+    //get years
+    let year1 = parseInt(date1.substring(7));
+    let year2 = parseInt(date2.substring(7));
+    if(year1 > year2)
+    {
+        return -1;
+    }
+    if(year1 < year2)
+    {
+        return 1;
+    }
+
+    //get months if years are equal
+    let month1 = 0;
+    switch(date1.substring(0,3)){
+        case "Jan":
+            month1 = 1;
+            break;
+        case "Feb":
+            month1 = 2;
+            break;
+        case "Mar":
+            month1 = 3;
+            break;
+        case "Apr":
+            month1 = 4;
+            break;
+        case "May":
+            month1 = 5;
+            break;
+        case "Jun":
+            month1 = 6;
+            break;
+        case "Jul":
+            month1 = 7;
+            break;
+        case "Aug":
+            month1 = 8;
+            break;
+        case "Sep":
+            month1 = 9;
+            break;
+        case "Oct":
+            month1 = 10;
+            break;
+        case "Nov":
+            month1 = 11;
+            break;
+        case "Dec":
+            month1 = 12;
+            break;
+    }
+    let month2 = 0;
+    switch(date2.substring(0,3)){
+        case "Jan":
+            month2 = 1;
+            break;
+        case "Feb":
+            month2 = 2;
+            break;
+        case "Mar":
+            month2 = 3;
+            break;
+        case "Apr":
+            month2 = 4;
+            break;
+        case "May":
+            month2 = 5;
+            break;
+        case "Jun":
+            month2 = 6;
+            break;
+        case "Jul":
+            month2 = 7;
+            break;
+        case "Aug":
+            month2 = 8;
+            break;
+        case "Sep":
+            month2 = 9;
+            break;
+        case "Oct":
+            month2 = 10;
+            break;
+        case "Nov":
+            month2 = 11;
+            break;
+        case "Dec":
+            month2 = 12;
+            break;
+    }
+    if(month1 > month2)
+    {
+        return -1;
+    }
+    if(month1 < month2)
+    {
+        return 1;
+    }
+
+    //get days if months are equal
+    let day1 = parseInt(date1.substring(4,6));
+    let day2 = parseInt(date2.substring(4,6));
+    if(day1 > day2)
+    {
+        return -1;
+    }
+    if(day1 < day2)
+    {
+        return 1
+    }
+    return 0;
+}
+
+function compareNums(num1,num2)
+{
+    return num2-num1;
 }
 
