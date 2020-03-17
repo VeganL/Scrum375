0be90c318282724dc26de181b1633cd5242b2f0e
Edward Parrish <elp49@drexel.edu>
2020-03-16 23:54:37 -0400

    Mobile view changes: added top navbar styles, added hamburger button and dropdown menu, added necessary javascript for dropdown menu, changed display style of content class to flex.

--- public/Register/register.js
+++ public/Register/register.js
@@ -1,3 +1,12 @@
+document.getElementById("hamburger").addEventListener("click", function (event) {
+   var dropdown = document.getElementById("dropdown");
+   if (dropdown.hidden) {
+       dropdown.hidden = false;
+   } else {
+       dropdown.hidden = true;
+   }
+});
+
 function validateForm() {
     let nameValid = false;
     let emailValid = false;
