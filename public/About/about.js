93abff01a06cbe0bb30e9169139ec4f8f2adda5f
VeganL <veganlroy@gmail.com>
2020-03-18 12:14:00 -0400

    20200318 1213: Trying same dynamic link thing on About page

--- public/About/about.js
+++ public/About/about.js
@@ -6,3 +6,7 @@ document.getElementById("hamburger").addEventListener("click", function (event) 
         dropdown.hidden = true;
     }
 });
+
+if (localStorage.getItem('username') !== null) {
+    document.getElementById('bars').innerHTML = '<div class="topbar"><h1>scrum375</h1><div id="dropdown" hidden><a href="../Boards/">Manage</a><a href="../Profile/' + localStorage.getItem('username') + '">Profile</a><a href="../About/">About</a></div><i id="hamburger" class="icon fa fa-bars"></i></div><div class="sidebar"><div class="vertical-center" style="text-align: center;"><h1 id="header-firstline">scrum</h1><h1 id="header-secondline">375</h1><a href="../Boards/">Manage</a><a href="../Profile/' + localStorage.getItem('username') + '">Profile</a><a href="../About/">About</a></div></div>';
+}
