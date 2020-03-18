1f5e72b97e27cc52a4bb757e32e74a5de4d3b064
VeganL <veganlroy@gmail.com>
2020-03-18 11:41:39 -0400

    20200318 1141: barSetter attempt 4

--- public/barSetter.js
+++ public/barSetter.js
@@ -1,19 +1,10 @@
-function setBars () {
-    let dropDownArea = document.getElementById('dropdown');
-    let sideBarArea = document.getElementsByClassName('sidebar');
-    let username = localStorage.getItem('username');
+let barArea = document.getElementById('bars');
+let username = localStorage.getItem('username');
 
-    if (username !== null) {
-        let dropDownStr = '<a href="../Boards/">Manage</a><a href="../Profile/' + username + '">Profile</a><a href="../About/">About</a>';
-        let sideBarStr = '<div class="vertical-center" style="text-align: center;"><h1 id="header-firstline">scrum</h1><h1 id="header-secondline">375</h1><a href="../Boards/">Manage</a><a href="../Profile/">Profile</a><a href="../About/">About</a></div>';
-
-        dropDownArea.innerHTML = dropDownStr;
-        sideBarArea.innerHTML = sideBarStr;
+barArea.addEventListener('load', function () {
+    if (username === null) {
+        barArea.innerHTML = '<div class="topbar"><h1>scrum375</h1><div id="dropdown" hidden><a href="../Register/">Register</a><a href="../Login/">Login</a><a href="../About/">About</a></div><i id="hamburger" class="icon fa fa-bars"></i></div><div class="sidebar"><div class="vertical-center" style="text-align: center;"><h1 id="header-firstline">scrum</h1><h1 id="header-secondline">375</h1><a href="../Register/">Register</a><a href="../Login/">Login</a><a href="../About/">About</a></div></div>';
     } else {
-        let dropDownStr = '<a href="../Register/">Register</a><a href="../Login/">Login</a><a href="../About/">About</a>';
-        let sideBarStr = '<div class="vertical-center" style="text-align: center;"><h1 id="header-firstline">scrum</h1><h1 id="header-secondline">375</h1><a href="../Register/">Register</a><a href="../Login/">Login</a><a href="../About/">About</a></div>';
-
-        dropDownArea.innerHTML = dropDownStr;
-        sideBarArea.innerHTML = sideBarStr;
+        barArea.innerHTML = '<div class="topbar"><h1>scrum375</h1><div id="dropdown" hidden><a href="../Boards/">Manage</a><a href="../Profile/">Profile</a><a href="../About/">About</a></div><i id="hamburger" class="icon fa fa-bars"></i></div><div class="sidebar"><div class="vertical-center" style="text-align: center;"><h1 id="header-firstline">scrum</h1><h1 id="header-secondline">375</h1><a href="../Boards/">Manage</a><a href="../Profile/' + username + '">Profile</a><a href="../About/">About</a></div></div>';
     }
-};
+});
